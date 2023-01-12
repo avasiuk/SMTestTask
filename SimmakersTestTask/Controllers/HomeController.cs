d8aff13a397798060e493203b9b702a587651395
avasiuk <fpm.vasyuk@gmail.com>
2023-01-12 16:49:11 +0300

    Create list of users

--- SimmakersTestTask/Controllers/HomeController.cs
+++ SimmakersTestTask/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SimmakersTestTask.Models;
+using SimmakersTestTask.ViewModels;
 using System.Diagnostics;
 
 namespace SimmakersTestTask.Controllers
@@ -7,15 +9,27 @@ namespace SimmakersTestTask.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var usersInfo = _userManager.Users.Select(p => new UserViewModel()
+
+            {
+                UserId = p.Id,
+                Username = p.UserName,
+                Email = p.Email,
+                Avatar = p.Avatar,
+                AvatarName = p.AvatarName
+            });
+
+            return View(usersInfo);
         }
 
         public IActionResult Privacy()
---
+++ SimmakersTestTask/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SimmakersTestTask.ViewModels
+{
+    public class UserViewModel
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string? AvatarName { get; set; }
+        public byte[]? Avatar { get; set; }
+    }
+}
